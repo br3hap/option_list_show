ef2c6a0aac09f0bc863a03c51921815ba27910a4
Breithner <Breithnerap@gmail.com>
2025-07-01 20:44:45 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Option_list_show',
+    'version': '',
+    'summary': """ Option_list_show Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
